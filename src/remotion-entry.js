d59b66cab4e3af6905f7695cac18e7c2ee308362
David Silverman <davidsilverman@Davids-MacBook-Pro-3.local>
2025-07-27 12:05:00 -0400

    Use pure JavaScript React.createElement to avoid JSX issues

--- src/remotion-entry.js
+++ src/remotion-entry.js
@@ -1,49 +1,42 @@
-const {
-  registerRoot,
-  Composition,
-  AbsoluteFill,
-} = require("remotion");
+const React = require("react");
+const { registerRoot, Composition, AbsoluteFill } = require("remotion");
 
 // Super simple test component
 const MainComposition = () => {
   console.log("MainComposition is being executed!");
   console.log("This is a test log from the Remotion component!");
 
-  return (
-    <AbsoluteFill
-      style={{
-        backgroundColor: "orange",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        color: "white",
-        fontSize: "80px",
-        fontWeight: "bold",
-      }}
-    >
-      <div>
-        <div>ORANGE TEST</div>
-        <div style={{ fontSize: "40px", marginTop: "20px" }}>
-          JS Component is working!
-        </div>
-      </div>
-    </AbsoluteFill>
+  return React.createElement(AbsoluteFill, {
+    style: {
+      backgroundColor: "orange",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      color: "white",
+      fontSize: "80px",
+      fontWeight: "bold",
+    }
+  }, 
+    React.createElement("div", {}, 
+      React.createElement("div", {}, "ORANGE TEST"),
+      React.createElement("div", { 
+        style: { fontSize: "40px", marginTop: "20px" } 
+      }, "JS Component is working!")
+    )
   );
 };
 
-// Export the component as named export
-module.exports = { MainComposition };
+// Export the component as default export
+module.exports = MainComposition;
 
 // Register the root component
 registerRoot(() => {
-  return (
-    <Composition
-      id="test-composition"
-      component={MainComposition}
-      durationInFrames={150}
-      fps={30}
-      width={1920}
-      height={1080}
-    />
-  );
-}); 
+  return React.createElement(Composition, {
+    id: "test-composition",
+    component: MainComposition,
+    durationInFrames: 150,
+    fps: 30,
+    width: 1920,
+    height: 1080
+  });
+});
